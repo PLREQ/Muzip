363f72b7880c1c4daf3ac175fb733454fd216525
yeni <dev.yeeun@gmail.com>
2023-06-11 02:08:41 +0900

    :card_file_box: CoreDataManager 메서드 추가

--- Muzip/Model/MuzipDataManager.swift
+++ Muzip/Model/MuzipDataManager.swift
@@ -16,6 +16,8 @@ class MuzipDataManager {
     var context: NSManagedObjectContext {
         return container.viewContext
     }
+    
+    /// Model Name
     let playListModelName: String = "PlayList"
     let MusicModelName: String = "Music"
 
@@ -31,8 +33,7 @@ class MuzipDataManager {
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
-    
-    
+
     /// PlayList 불러오기
     func fetch() -> [NSManagedObject] {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: playListModelName)
@@ -49,33 +50,97 @@ class MuzipDataManager {
         }
     }
     
+    /// 플레이리스트 저장
+    func savePlayList(title: String, location: String, day: Date, latitude: Double, longtitude: Double, musics: [Music]) {
+        let playListObject = NSEntityDescription.insertNewObject(forEntityName: playListModelName, into: context)
+        playListObject.setValue(title, forKey: "title")
+        playListObject.setValue(day, forKey: "day")
+        playListObject.setValue(location, forKey: "location")
+        playListObject.setValue(latitude, forKey: "latitude")
+        playListObject.setValue(longtitude, forKey: "longtitude")
+
+        let group = DispatchGroup()
+        var errorOccurred = false
+
+        for music in musics {
+            if music.title != "" { // 음악 검색을 실패하면 저장되지 않는다.
+                let musicObject = NSEntityDescription.insertNewObject(forEntityName: MusicModelName, into: context) as! MusicDB
+                musicObject.title = music.title
+                musicObject.artist = music.artist
+
+                group.enter()
+                DispatchQueue.global().async {
+                    if let data = try? Data(contentsOf: music.musicImageURL) {
+                        if let image = UIImage(data: data) {
+                            musicObject.musicImage = image.jpegData(compressionQuality: 1.0)
+                        }
+                    }
+                    (playListObject as! PlayListDB).addMusicToPlayList(musicObject)
+                    group.leave()
+                }
+            }
+        }
+
+        group.notify(queue: DispatchQueue.global()) {
+            do {
+                try self.context.save()
+            } catch {
+                print("failure Save")
+            }
+        }
+    }
     
-    /// 저장된 PlayList 삭제
-    func deletePlayList(){
-        saveContext()
+    
+    /// playList 삭제
+    func deletePlayList(playListObject: NSManagedObject) -> Bool {
+        context.delete(playListObject)
+
+        return saveContext()
     }
 
-    /// PlayList에 곡들을 추가
-    func addMusicToPlayList(playListObject: NSManagedObject, musics: [MusicData]) -> Bool {
+    /// platList의 제목 변경
+    func updatePlayListTitle(playListObject: NSManagedObject, title: String) -> Bool {
+        playListObject.setValue(title, forKey: "title")
+
+        return saveContext()
+    }
+
+    /// playLisy에서 music들을 불러오기
+    func musicsFetch(playList: PlayListDB) -> [MusicDB] {
+        return playList.music?.array as! [MusicDB]
+    }
+
+    /// playList에 music들을 추가
+    func addMusicsToPlayList(playListObject: NSManagedObject, musics: [MusicData]) -> Bool {
         for music in musics {
             let musicObject = NSEntityDescription.insertNewObject(forEntityName: MusicModelName, into: context) as! MusicDB
             musicObject.title = music.title
             musicObject.artist = music.artist
             musicObject.musicImage = music.musicImage
             
-//            (playListObject as! PlayListDB).addToMusic(musicObject)
+            (playListObject as! PlayListDB).addMusicToPlayList(musicObject)
         }
         return saveContext()
     }
-
     
-    /// 곡 삭제
+    /// playList에서 music 삭제
     func deleteMusic(music: NSManagedObject){
         context.delete(music)
         saveContext()
     }
     
-    ///
+    /// playList 안에서 music의 순서를 변경
+    func changeMusicOrder(playListObject: [NSManagedObject], musics: [MusicData]) -> Bool {
+        for i in 0..<playListObject.count {
+            playListObject[i].setValue(musics[i].title, forKey: "title")
+            playListObject[i].setValue(musics[i].artist, forKey: "artist")
+            playListObject[i].setValue(musics[i].musicImage, forKey: "musicImage")
+        }
+
+        return saveContext()
+    }
+    
+    /// 상태 저장
     func saveContext() -> Bool{
         if context.hasChanges{
             do {
@@ -84,10 +149,8 @@ class MuzipDataManager {
             } catch {
                 let nserror = error as NSError
                 fatalError("Unsolved error \(nserror), \(nserror.userInfo)")
-                return false
             }
-        } else {
-            return false
         }
+        return false
     }
 }
